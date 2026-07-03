d41c870c61551cdfa07f0b0f5c4fa866b4fa8c2d
znine825 <earthonemen@gmail.com>
2026-07-03 22:36:43 +0000

    2026-07-04 edit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react'
 import './Header.css'
 
-function Header() {
+
+function Header(where) {
     return (
-        <div>
+        <div className = 'header'>
            <p>비체담 웹사이트 리빌딩 제안서</p>
-           <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+           <div classNema = 'menu'>
+                <div><p>목차</p></div>
+                <div><p>개요</p></div>
+                <div><p>진단</p></div>
+                <div><p>방향</p></div>
+                <div><p>디자인</p></div>
+                <div><p>일정</p></div>
            </div>
         </div>
     )
