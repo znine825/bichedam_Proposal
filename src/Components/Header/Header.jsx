0204280e9593a8945636e4b588be04a61853a4e9
ZNINE <earthonemen@gmail.com>
2026-07-03 22:59:40 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   src/Components/Header/Header.css
    	new file:   src/Components/Header/Header.jsx
    	new file:   src/Page/1.Outline/Outline.css
    	new file:   src/Page/1.Outline/Outline.jsx
    	modified:   src/index.css
    	modified:   src/main.jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+           <p>비체담 웹사이트 리빌딩 제안서</p>
+           <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+           </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Page/1.Outline/Outline.jsx
@@ -0,0 +1,12 @@
+import './Outline.css'
+import Header from '../../Components/Header/Header.jsx'
+
+function Outline() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Outline
