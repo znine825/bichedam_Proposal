0b4de56754813420fff3dbfb6ca63588a5e24687
znine825 <earthonemen@gmail.com>
2026-07-05 06:08:42 +0900

    2026-07-05 edit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react()
   ],
   base: '/',
 })
