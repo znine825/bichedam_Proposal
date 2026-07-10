b634a42d07ebff11b99336407d86a6309a800478
znine825 <earthonemen@gmail.com>
2026-07-10 19:06:21 +0900

    Remove Babel plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react()
   ],
   base: '/',
 })
