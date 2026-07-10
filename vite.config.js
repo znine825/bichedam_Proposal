65248d662f0a9fb8fc50d93c8705e25c7c7744df
znine825 <earthonemen@gmail.com>
2026-07-10 19:05:05 +0900

    Update Vite config to include Babel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/bichedam_Proposal/',
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  base: '/',
 })
